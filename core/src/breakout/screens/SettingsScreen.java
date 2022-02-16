0d5f2bce1fd7ebd56d82c696b9ce47da3d8f64aa
Brendan Donohue <brendandonohue2022@gmail.com>
2022-02-15 19:01:43 -0800

    Fix drop-down menu for theme selection

--- core/src/breakout/screens/SettingsScreen.java
+++ core/src/breakout/screens/SettingsScreen.java
@@ -8,8 +8,10 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class SettingsScreen extends ScreenAdapter {
@@ -26,19 +28,36 @@ public class SettingsScreen extends ScreenAdapter {
         this.game = game;
         batch = new SpriteBatch();
         skin = new Skin(Gdx.files.internal("neon/skin/neon-ui.json"));
+
         Dialog dialog = new Dialog("Setting",skin);
         dialog.setSize(300,300);
-        dialog.setPosition(Gdx.graphics.getWidth()/2-100,Gdx.graphics.getHeight()/2-100);
+        dialog.setPosition(Gdx.graphics.getWidth() / 2.f - 100,Gdx.graphics.getHeight() / 2.f - 100);
+
         selectBox = new SelectBox<>(skin);
         selectBox.setItems("Default", "Weezer");
+
+        selectBox.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                switch (selectBox.getSelectedIndex()) {
+
+                    case 0 -> game.disableWeezerMode();
+                    case 1 -> game.enableWeezerMode();
+                }
+            }
+        });
+
         dialog.getContentTable().defaults().pad(10);
         dialog.getContentTable().add(selectBox);
-        Gdx.input.setInputProcessor(stage);
+
         exitButton = new Rectangle(50, 150, 200, 100);
         exitTexture = new Texture(Gdx.files.internal("exitbutton.png"));
+
         bg = new Texture(Gdx.files.internal("menu bg.png"));
+
         stage = new Stage();
         stage.addActor(dialog);
+
         Gdx.input.setInputProcessor(stage);
     }
 
@@ -54,6 +73,7 @@ public class SettingsScreen extends ScreenAdapter {
 
     public void render (float dt) {
         ScreenUtils.clear(Color.BLACK);
+        stage.act();
         batch.begin();
         batch.draw(bg, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         batch.draw(exitTexture, exitButton.x, exitButton.y);
